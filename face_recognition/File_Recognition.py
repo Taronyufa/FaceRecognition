82aab41e3709fdf62323ee796b38f97732b8bd3c
Taronyufa <emanuele.stefani.geo@gmail.com>
2023-05-08 09:32:06 +0200

    Adding File recognition

---
+++ face_recognition/File_Recognition.py
@@ -0,0 +1,33 @@
+import numpy
+import cv2
+import face_recognition
+
+# Taking the files and changing the color scheme from bgr to rgb
+imgBill = face_recognition.load_image_file('Data/Bill Gates.jpg')
+imgBill = cv2.cvtColor(imgBill, cv2.COLOR_BGR2RGB)
+
+imgTest = face_recognition.load_image_file('Data/Gates Test.jpg')
+imgTest = cv2.cvtColor(imgTest, cv2.COLOR_BGR2RGB)
+
+# finding the location of the face
+faceLocation = face_recognition.face_locations(imgBill)[0]
+encodeBill = face_recognition.face_encodings(imgBill)[0]
+
+locationTest = face_recognition.face_locations(imgTest)[0]
+encodeTest = face_recognition.face_encodings(imgTest)[0]
+
+# finds if both imgs are the same person and the distance between them
+similarities = face_recognition.compare_faces([encodeBill], encodeTest)
+distance = face_recognition.face_distance([encodeBill], encodeTest)
+
+# drawing the rectangle in the photo
+cv2.rectangle(imgBill, (faceLocation[3], faceLocation[0]), (faceLocation[1], faceLocation[2]), (0,0,255), 3)
+cv2.rectangle(imgTest, (locationTest[3], locationTest[0]), (locationTest[1], locationTest[2]), (0,0,255), 3)
+
+cv2.putText(imgTest, f'{similarities} |  {round(distance[0], 2)}', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0,0,255), 1)
+
+# showing the imgs and waiting for a key to be pressed to close it
+# cv2.imshow('Bill', imgBill)
+cv2.imshow('Test', imgTest)
+
+cv2.waitKey(0)
